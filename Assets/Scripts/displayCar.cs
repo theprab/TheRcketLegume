d39ed9a7d968a2d26518bb2a57b7d9049e7798ec
thondrill <prabu.thendral03@gmail.com>
2021-06-18 15:20:25 -0400

    created welcome screen, adjusted intermission screen

---
+++ Assets/Scripts/displayCar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayCar : MonoBehaviour
+{
+    Vector3 initialPos = new Vector3();
+    Vector3 tempPos = new Vector3();
+    void Start()
+    {
+        initialPos = transform.position; //need this initial position for later
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f,Time.deltaTime * 3.0f, 0.0f);
+    }
+}
