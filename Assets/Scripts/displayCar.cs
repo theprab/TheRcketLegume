d805d7c99a95200347542a5a29db71a1460206f9
thondrill <prabu.thendral03@gmail.com>
2021-06-20 22:44:48 -0400

    attempts to get welcome car to spin, restore movement in rckt scene

--- Assets/Scripts/displayCar.cs
+++ Assets/Scripts/displayCar.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class displayCar : MonoBehaviour
 {
-    Vector3 initialPos = new Vector3();
     void Start()
     {
-        initialPos = transform.position; //need this initial position for later
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.0f, Time.deltaTime * 50, 0.0f);
+        transform.Rotate(0.0f, 50 * Time.deltaTime, 0.0f);
     }
 }
