7ecda13dd386cbd79ac9f344b2e8816b3ac417ee
thondrill <prabu.thendral03@gmail.com>
2021-07-05 22:24:06 -0400

    started working on win story and it looks FANTASTIC

--- Assets/Scripts/displayCar.cs
+++ Assets/Scripts/displayCar.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class displayCar : MonoBehaviour
 {
     public GameObject car;
+    public float spinSpeed;
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(car.name);
-        car.transform.Rotate(0.0f, 2, 0.0f);
+        car.transform.Rotate(0.0f, spinSpeed, 0.0f);
     }
 }
