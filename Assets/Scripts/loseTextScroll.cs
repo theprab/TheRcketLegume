92bb3f57518a57a59ed2f03ad0917f7c9edfad7a
thondrill <prabu.thendral03@gmail.com>
2021-08-15 11:22:36 -0400

    experimenting with scrolling text in lose story 2

---
+++ Assets/Scripts/loseTextScroll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loseTextScroll : MonoBehaviour
+{
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //initially text1 will set active and text2, text3 set inactive
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            text1.SetActive(false);
+            text2.SetActive(true);
+            /*if (Input.GetKeyDown(KeyCode.Space) && text1.activeInHierarchy == false) {
+                text2.SetActive(false);
+                text3.SetActive(true);
+            }*/
+        }
+    }
+}
