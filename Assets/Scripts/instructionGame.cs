2ee0f07410d0506c5e790f37730b39efcac44426
theprab <prabut1@tcnj.edu>
2022-08-26 19:21:55 -0500

    start text with time added and lose screen adjusted

---
+++ Assets/Scripts/instructionGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class instructionGame : MonoBehaviour
+{
+    public float duration = 4f;
+    public int timeLeft;
+    public GameObject aMenu;
+
+    public void Awake()
+    {
+        aMenu.SetActive(true);
+    }
+    void Start()
+    {
+        aMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        duration -= Time.deltaTime;
+        if (duration > 0)
+        {
+            Debug.Log(duration);
+        } else
+        {
+            aMenu.SetActive(false);
+        }
+
+
+    }
+
+
+}
