e8a53ce518c36955656fd075032143cd639c9cbf
theprab <prabut1@tcnj.edu>
2022-08-27 13:19:39 -0500

    added start instructions to gameplay scene and adjusted UI

---
+++ Assets/Scripts/startInstruction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startInstruction : MonoBehaviour
+{
+    public float duration = 3f;
+    public GameObject aMenu;
+    public bool isStarted = false;
+    void Awake()
+    {
+        aMenu.SetActive(true);
+    }
+    void Start()
+    {
+        aMenu.SetActive(true);
+        isStarted = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        duration -= Time.deltaTime;
+        Debug.Log(duration);
+        if (duration <= 0 || Input.GetKeyDown(KeyCode.Space)) {
+            aMenu.SetActive(false);
+            isStarted = true;
+        }
+        if (isStarted)
+        {
+            aMenu.SetActive(false);
+        }
+    }
+}
