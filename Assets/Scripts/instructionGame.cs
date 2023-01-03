38bb0126c80ee4db938205329a044c780b4f61f6
theprab <prabut1@tcnj.edu>
2023-01-03 11:38:15 -0500

    reimported/updated assets, tried adjustments to start instructions but did not work

--- Assets/Scripts/instructionGame.cs
+++ Assets/Scripts/instructionGame.cs
@@ -8,8 +8,14 @@ public class instructionGame : MonoBehaviour
     public float duration = 4f;
     public int timeLeft;
     public GameObject aMenu;
+<<<<<<< Updated upstream:Assets/Scripts/instructionGame.cs
 
     public void Awake()
+=======
+    public bool isStarted = false;
+    public int keyPressCount = 0;
+    void Awake()
+>>>>>>> Stashed changes:Assets/Scripts/startInstruction.cs
     {
         aMenu.SetActive(true);
     }
@@ -22,10 +28,26 @@ public class instructionGame : MonoBehaviour
     void Update()
     {
         duration -= Time.deltaTime;
+<<<<<<< Updated upstream:Assets/Scripts/instructionGame.cs
         if (duration > 0)
         {
             Debug.Log(duration);
         } else
+=======
+        Debug.Log(duration);
+        while(keyPressCount <= 2)
+        {
+            //Debug.Log("key down" + Input.GetKeyDown(KeyCode.Escape));
+            Debug.Log("keyPressCount before" + keyPressCount);
+            if (Input.GetKeyDown(KeyCode.Escape) && Input.GetKeyUp(KeyCode.Escape)) { keyPressCount++; }
+            Debug.Log("keyPressCount after" + keyPressCount);
+        }
+        if (duration <= 0 || Input.GetKeyDown(KeyCode.Space)) {
+            aMenu.SetActive(false);
+            isStarted = true;
+        }
+        if (isStarted)
+>>>>>>> Stashed changes:Assets/Scripts/startInstruction.cs
         {
             aMenu.SetActive(false);
         }
